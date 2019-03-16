b5581fb34327ba1b9efd3cd539cc4898e86a0ae0
yzh_Dian <2169218186@qq.com>
2019-03-16 21:44:42 +0800

    完成所有的逻辑

--- app/src/main/java/e/yizii/sie/MainActivity.java
+++ app/src/main/java/e/yizii/sie/MainActivity.java
@@ -1,15 +1,19 @@
 package e.yizii.sie;
 
-
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.hardware.Camera;
 import android.os.Build;
-import android.os.SystemClock;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+
+import java.io.IOException;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -17,12 +21,16 @@ public class MainActivity extends AppCompatActivity {
     private Camera mCamera = null;
     private Camera.Parameters mParameters = null;
     boolean buttonSet = true;
+    private static final String TAG = "MainActivity";
+    private Button button;
+    private ImageView imageView;
+    private Boolean Stop = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final Button button = findViewById(R.id.button);
+        button = findViewById(R.id.button);
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             if (checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
                 requestPermissions(new String[] {Manifest.permission.CAMERA}, 1);
@@ -30,26 +38,60 @@ public class MainActivity extends AppCompatActivity {
         }
 
 
-
+        imageView = findViewById(R.id.image1);
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 if (buttonSet == true)
                 {
-                    button.setText("关闭闪关灯");
-                    for (int i =0; i < 10; i++)
-                    {
-                        startCamera();
-//                        SystemClock.sleep(1000);
+                    button.setText("关闭");
+                    Stop = false;
+                    Font16 font16 = new Font16(getApplication().getApplicationContext());
+                    try {
+                        final boolean[][] arr = font16.drawString("我们");
 
+                        imageView.setVisibility(View.VISIBLE);
+                        Looper looper = Looper.myLooper();//取得当前线程里的looper
 
+                        final MyHandler mHandler = new MyHandler(looper);//构造一个handler使之可与looper通信
+                        final String[] msgStr = {""};
+                        Thread thread = new Thread(){
+                            @Override
+                            public void run(){
+                                for (int i = 0; i < arr.length && Stop == false; i++)
+                                {
+                                    for (int j = 0; j < arr[0].length; j++) {
+                                        mHandler.removeMessages(0);
+                                        if (arr[i][j] = true)
+                                        {
+                                            msgStr[0] = "black";
+                                        }else {
+                                            msgStr[0] = "white";
+                                        }
+                                        Message m = mHandler.obtainMessage(1, 1, 1, msgStr[0]);//构造要传递的消息
+                                        mHandler.sendMessage(m);//发送消息:系统会自动调用handleMessage方法来处理消息
+                                        try {
+                                            Thread.sleep(100);
+                                        } catch (InterruptedException e) {
+                                            e.printStackTrace();
+                                        }
+                                    }
+                                }
+                            }
+                        };
+
+                        thread.start();
+                        buttonSet = false;
+                    } catch (IOException e) {
+                        e.printStackTrace();
                     }
 
-                    buttonSet = false;
                 }else {
 
-                    button.setText("打开闪光灯");
-                    closeCamera();
+                    button.setText("打开");
+                    imageView.setVisibility(View.INVISIBLE);
+//                    closeCamera();
                     buttonSet = true;
+                    Stop = true;
                 }
 
             }
@@ -71,4 +113,36 @@ public class MainActivity extends AppCompatActivity {
         mCamera.release();
         mCamera = null;
     }
+
+    private class MyHandler extends Handler{
+     public MyHandler(Looper looper){
+         super(looper);
+        }
+
+        @Override
+        public void handleMessage(Message msg) {//处理消息
+         if (msg.obj.toString().equals("white"))
+         {
+             imageView.setImageResource(R.drawable.withe);
+         }else {
+             imageView.setImageResource(R.drawable.timg);
+         }
+//            Log.d(TAG, "handleMessage: " + msg.obj.toString());
+//            text.setText(msg.obj.toString());
+        }
+
+    }
+
+//    class Mythread extends Thread{
+//        Mythread(){
+//
+//        }
+//
+//        @Override
+//        public void run(){
+//
+//        }
+//    }
+//
+
 }
